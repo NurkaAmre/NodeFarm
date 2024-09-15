9690a11e67cfb767f229a0197cfb7be0160de09f
NurkaAmre <nurkerey30@gmail.com>
2024-09-15 15:16:06 +0600

    Filling the templates

--- starter/index.js
+++ starter/index.js
@@ -39,6 +39,20 @@ const url = require('url');
 
 /////////////////////////////////////////////
 // SERVER
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template_overview.html`,
   'utf-8'
@@ -65,6 +79,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+
     res.end(tempOverview);
     //Product page
   } else if (pathName === '/product') {
